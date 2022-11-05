d41e3b7ca1772a8d44cc8d699f9770df09120cc5
Lea Foy <leafoy@gmail.com>
2022-11-05 17:42:12 -0400

    Add elements

--- dictionary.py
+++ dictionary.py
@@ -1,19 +1,31 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QFileDialog
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QFileDialog, QLineEdit
 from PyQt6.QtCore import Qt
 from pathlib import Path
 
+def word_look_up():
+    pass
+    
 
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle("Dictionary")
 layout = QVBoxLayout()
 
+# Top label
 description = QLabel('Search for a definition:')
 layout.addWidget(description)
 
+# Input for word 
+input = QLineEdit()
+layout.addWidget(input)
 
-message = QLabel("")
-layout.addWidget(message, alignment=Qt.AlignmentFlag.AlignCenter)
+# Search button
+button = QPushButton("Look up word")
+layout.addWidget(button)
+button.clicked.connect(word_look_up)
+
+definition = QLabel("")
+layout.addWidget(definition, alignment=Qt.AlignmentFlag.AlignCenter)
 
 window.setLayout(layout)
 window.show()
